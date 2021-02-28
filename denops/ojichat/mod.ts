4a7b098053383f9d6b2234d63fa8c61a760b8b91
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2021-02-28 16:58:20 +0900

    mod type guard function

--- denops/ojichat/mod.ts
+++ denops/ojichat/mod.ts
@@ -2,24 +2,24 @@ import { start } from "https://deno.land/x/denops_std@v0.3/mod.ts";
 import ojichat from "https://cdn.skypack.dev/ojichat.js@0.0.6?dts";
 import { parse } from "https://deno.land/std@0.88.0/flags/mod.ts";
 
+function isStringArray(obj: unknown): obj is Array<string> {
+  return Array.isArray(obj) && obj.every((value) => typeof value === "string");
+}
+
 start(async (vim) => {
   vim.register({
-    async run(args: unknown): Promise<void> {
+    async run(...args: unknown[]): Promise<void> {
       const yankReg = await vim.v.get("register");
 
-      let target: string | undefined;
-      let emoji: number | undefined;
-      let yank: boolean | undefined;
-
-      if (Array.isArray(args)) {
-        const parsedArgs = parse(args, { "--": true });
-
-        target = parsedArgs._.join(" ");
-        emoji = parsedArgs.e ?? parsedArgs.emoji;
-        yank = parsedArgs.yank;
-      } else if (args != null) {
-        throw new Error(`'args' in 'run()' of ${vim.name} must be a string`);
+      if (!isStringArray(args)) {
+        throw Error();
       }
+
+      const parsedArgs = parse(args, { "--": true });
+
+      const target: string = parsedArgs._.join(" ");
+      const emoji: number = parsedArgs.e ?? parsedArgs.emoji;
+      const yank: boolean = parsedArgs.yank;
 
       const message = new ojichat.Generator(target, emoji).getMessage();
 
@@ -35,6 +35,6 @@ start(async (vim) => {
     },
   });
   await vim.execute(`
-    command! -nargs=* DenopsOjichat echo denops#notify("${vim.name}", "run", [[<f-args>]])
+    command! -nargs=* DenopsOjichat echo denops#notify("${vim.name}", "run", [<f-args>])
   `);
 });
