e05195c6ebde85a07fec39be2337dc02d8139cc3
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2021-02-28 17:16:42 +0900

    remove type guard function

--- denops/ojichat/mod.ts
+++ denops/ojichat/mod.ts
@@ -2,20 +2,12 @@ import { start } from "https://deno.land/x/denops_std@v0.3/mod.ts";
 import ojichat from "https://cdn.skypack.dev/ojichat.js@0.0.6?dts";
 import { parse } from "https://deno.land/std@0.88.0/flags/mod.ts";
 
-function isStringArray(obj: unknown): obj is Array<string> {
-  return Array.isArray(obj) && obj.every((value) => typeof value === "string");
-}
-
 start(async (vim) => {
   vim.register({
     async run(...args: unknown[]): Promise<void> {
       const yankReg = await vim.v.get("register");
 
-      if (!isStringArray(args)) {
-        throw Error();
-      }
-
-      const parsedArgs = parse(args, { "--": true });
+      const parsedArgs = parse(args as string[], { "--": true });
 
       const target: string = parsedArgs._.join(" ");
       const emoji: number = parsedArgs.e ?? parsedArgs.emoji;
